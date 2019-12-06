1e18709ca61935d4f26876664b75eccc017b9b82
JUNGEEYOU <junge2u@gmail.com>
2019-12-06 15:52:15 +0900

    command 닌텐도 수정

--- nintendo/AnimalForest.py
+++ nintendo/AnimalForest.py
@@ -10,25 +10,25 @@ class AnimalForest(object):
         앞으로 나가기
         :return:
         """
-        print("앞으로 나간다")
+        print("동물의 숲 - 앞으로 나가는 메소드 호출 ")
 
     def back(self):
         """
         뒤로 가기
         :return:
         """
-        print("뒤로 간다.")
+        print("동물의 숲 - 뒤로 가는 메소드 호출 ")
 
     def left(self):
         """
         오른쪽으로 이동
         :return:
         """
-        print("왼쪽으로 간다.")
+        print("동물의 숲 -왼쪽으로 가는 메소드 호출 ")
 
     def right(self):
         """
         오른쪽으로 이동
         :return:
         """
-        print("오른쪽으로 간다.")
+        print("동물의 숲 -오른쪽으로 가는 메소드 호출")
